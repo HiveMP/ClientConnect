db5eb584133afbfe65bf6682a0cee4663d7eeeca
June Rhodes <jrhodes@redpointgames.com.au>
2017-08-27 02:20:04 +1000

    Fix Steam DLL name

--- HiveMP.SteamTest/CopySteamApiDlls.ps1
+++ HiveMP.SteamTest/CopySteamApiDlls.ps1
@@ -4,4 +4,4 @@ param()
 # location. If you are building the Client Connect SDK yourself, you will need
 # to download the Steam SDK (we can't redistribute it) and copy the DLLs in place.
 Copy-Item -Force C:\Users\Redpoint\Documents\SteamworksSDK138a\sdk\redistributable_bin\steam_api.dll $PSScriptRoot\
-Copy-Item -Force C:\Users\Redpoint\Documents\SteamworksSDK138a\sdk\redistributable_bin\win64\steam_api.dll $PSScriptRoot\
+Copy-Item -Force C:\Users\Redpoint\Documents\SteamworksSDK138a\sdk\redistributable_bin\win64\steam_api64.dll $PSScriptRoot\
