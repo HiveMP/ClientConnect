ab39d2af668c4b1873a3889835dbc6121e757831
June Rhodes <jrhodes@redpointgames.com.au>
2017-09-05 13:35:53 +1000

    Add build for macOS

---
+++ build.mac.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd "$(dirname "$0")"
+
+if [ -f buildmac32 ]; then
+    rm -Rf buildmac32
+fi
+mkdir buildmac32
+cd buildmac32
+cmake -G "Xcode" -D CMAKE_OSX_ARCHITECTURES=i386 -D OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include ..
+xcodebuild -project HiveMP.ClientConnect.xcodeproj -configuration Release build
+
+cd "$(dirname "$0")"
+
+if [ -f buildmac64 ]; then
+    rm -Rf buildmac64
+fi
+mkdir buildmac64
+cd buildmac64
+cmake -G "Xcode" -D CMAKE_OSX_ARCHITECTURES=x86_64 -D OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include ..
+xcodebuild -project HiveMP.ClientConnect.xcodeproj -configuration Release build
