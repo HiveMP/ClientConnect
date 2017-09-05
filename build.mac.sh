f472cca7307aae482cd8e0c31f4eea48be46e94e
June Rhodes <jrhodes@redpointgames.com.au>
2017-09-05 16:08:26 +1000

    Use SHA1 hash of CMakeLists.txt file to determine when to discard CMake cache

--- build.mac.sh
+++ build.mac.sh
@@ -8,25 +8,26 @@ cd "$(dirname "$0")"
 sed -i -e 's/add_subdirectory\(docs\)/#add_subdirectory\(docs\)/g' curl/CMakeLists.txt
 
 # Builds are faster if we don't clear the CMake cache.
+SHASUM=$(shasum CMakeLists.txt | awk '{print $1}')
 
-#if [ -d buildmac32 ]; then
-#    rm -Rf buildmac32
-#fi
-if [ ! -d buildmac32 ]; then
-    mkdir buildmac32
+if [ -d buildmac32_$SHASUM ]; then
+    rm -Rf buildmac32_$SHASUM
 fi
-cd buildmac32
+if [ ! -d buildmac32_$SHASUM ]; then
+    mkdir buildmac32_$SHASUM
+fi
+cd buildmac32_$SHASUM
 cmake -G "Xcode" -D CMAKE_OSX_ARCHITECTURES=i386 -D OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include ..
 xcodebuild -project HiveMP.ClientConnect.xcodeproj -configuration Release build
 
 cd "$(dirname "$0")"
 
-if [ -d buildmac64 ]; then
-    rm -Rf buildmac64
+if [ -d buildmac64_$SHASUM ]; then
+    rm -Rf buildmac64_$SHASUM
 fi
-if [ ! -d buildmac64 ]; then
-    mkdir buildmac64
+if [ ! -d buildmac64_$SHASUM ]; then
+    mkdir buildmac64_$SHASUM
 fi
-cd buildmac64
+cd buildmac64_$SHASUM
 cmake -G "Xcode" -D CMAKE_OSX_ARCHITECTURES=x86_64 -D OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include ..
 xcodebuild -project HiveMP.ClientConnect.xcodeproj -configuration Release build
