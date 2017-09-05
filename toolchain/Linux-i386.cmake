7e6f6f33e97fea02f6284a9bb41bf81fc6c3ff03
June Rhodes <jrhodes@redpointgames.com.au>
2017-09-05 22:17:06 +1000

    Update toolchain files again

--- toolchain/Linux-i386.cmake
+++ toolchain/Linux-i386.cmake
@@ -1,8 +1,4 @@
-set(CMAKE_SYSTEM_NAME Linux)
-
-set(CMAKE_C_COMPILER gcc)
 set(CMAKE_C_FLAGS -m32)
-set(CMAKE_CXX_COMPILER g++)
 set(CMAKE_CXX_FLAGS -m32)
 
 set(OPENSSL_INCLUDE_DIR /usr/include/i386-linux-gnu)
--- toolchain/Linux-x86_64.cmake
+++ toolchain/Linux-x86_64.cmake
@@ -1,8 +1,4 @@
-set(CMAKE_SYSTEM_NAME Linux)
-
-set(CMAKE_C_COMPILER gcc)
 set(CMAKE_C_FLAGS -m64)
-set(CMAKE_CXX_COMPILER g++)
 set(CMAKE_CXX_FLAGS -m64)
 
 set(OPENSSL_INCLUDE_DIR /usr/include/x86_64-linux-gnu)
