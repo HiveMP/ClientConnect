b0a59e9a8023672d832436bf36c00854a7705f4e
June Rhodes <jrhodes@redpointgames.com.au>
2017-09-06 14:57:21 +1000

    Don't build 32-bit macOS binaries at all
    Apple is dropping support for 32-bit macOS binaries after High Sierra (https://www.imore.com/after-high-sierra-32-bit-games-mac-are-kicked-curb-0) and Lua FFI doesn't work on 32-bit macOS (https://github.com/jmckaskill/luaffi/issues/76). So just drop 32-bit macOS entirely as there's no value in shipping it in new software.

--- build.mac.sh
+++ build.mac.sh
@@ -13,13 +13,6 @@ sed -i -e 's/add_subdirectory\(docs\)/#add_subdirectory\(docs\)/g' curl/CMakeLis
 SHASUM=$(shasum CMakeLists.txt | awk '{print $1}')
 VERSION=v2
 
-if [ ! -d buildmac32_${SHASUM}_${VERSION} ]; then
-    mkdir buildmac32_${SHASUM}_${VERSION}
-fi
-cd buildmac32_${SHASUM}_${VERSION}
-cmake -G "Xcode" -D CMAKE_OSX_ARCHITECTURES=i386 -D OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include ..
-xcodebuild -project HiveMP.ClientConnect.xcodeproj -configuration Release build
-
 cd $ROOT
 
 if [ ! -d buildmac64_${SHASUM}_${VERSION} ]; then
@@ -28,14 +21,6 @@ fi
 cd buildmac64_${SHASUM}_${VERSION}
 cmake -G "Xcode" -D CMAKE_OSX_ARCHITECTURES=x86_64 -D OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include ..
 xcodebuild -project HiveMP.ClientConnect.xcodeproj -configuration Release build
-
-echo "Testing 32-bit binaries..."
-cd $ROOT/buildmac32_${SHASUM}_${VERSION}/Release
-./HiveMP.SteamTest | tee result.txt
-if [ "$(cat result.txt | grep -c "TEST PASS")" != "2" ]; then
-    echo "Test failed!"
-    exit 1
-fi
 
 echo "Testing 64-bit binaries..."
 cd $ROOT/buildmac64_${SHASUM}_${VERSION}/Release
@@ -51,9 +36,6 @@ echo "Creating distribution structure..."
 if [ -d dist ]; then
     rm -Rf dist
 fi
-mkdir -pv dist/sdk/Mac32
 mkdir -pv dist/sdk/Mac64
-cp buildmac32_${SHASUM}_${VERSION}/Release/libcurl.dylib dist/sdk/Mac32/
-cp buildmac32_${SHASUM}_${VERSION}/Release/libHiveMP.ClientConnect.dylib dist/sdk/Mac32/
 cp buildmac64_${SHASUM}_${VERSION}/Release/libcurl.dylib dist/sdk/Mac64/
 cp buildmac64_${SHASUM}_${VERSION}/Release/libHiveMP.ClientConnect.dylib dist/sdk/Mac64/
